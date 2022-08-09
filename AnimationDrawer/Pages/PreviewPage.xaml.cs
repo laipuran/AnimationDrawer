e5cbbb58cf3548f0a52ea70b35d77f503ed6251f
laipuran <55612059+laipuran@users.noreply.github.com>
2022-08-09 15:57:05 +0800

    Sliders were added

--- AnimationDrawer/Pages/PreviewPage.xaml.cs
+++ AnimationDrawer/Pages/PreviewPage.xaml.cs
@@ -23,10 +23,12 @@ namespace AnimationDrawer.Pages
     public partial class PreviewPage : Page
     {
         int FPS = 60;
+        int index = 0;
         public PreviewPage()
         {
             InitializeComponent();
             PreviewCanvas.Strokes = new();
+            FrameSlider.Maximum = App.strokes.Count - 1;
         }
 
         private void PlayButton_Click(object sender, RoutedEventArgs e)
@@ -40,9 +42,23 @@ namespace AnimationDrawer.Pages
         {
             foreach (StrokeCollection item in App.strokes)
             {
-                await Task.Run(() => Dispatcher.BeginInvoke(new Action(() => { PreviewCanvas.Strokes = item; })));
+                index = App.strokes.IndexOf(item);
+                await Task.Run(() => Dispatcher.BeginInvoke(new Action(() => { FrameSlider.Value = index; })));
                 await Task.Delay(1000 / FPS);
             }
+        }
+
+        private void FpsSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            FPS = (int)FpsSlider.Value;
+            FpsTextBlock.Text = "帧率：" + FPS.ToString();
+        }
+        
+        private async void FrameSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            index = (int)FrameSlider.Value;
+            await Task.Run(() => Dispatcher.BeginInvoke(new Action(() => { PreviewCanvas.Strokes = App.strokes[index]; })));
+            await Task.Run(() => Dispatcher.BeginInvoke(new Action(() => { FrameTextBlock.Text = index.ToString(); })));
         }
     }
 }
