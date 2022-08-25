24cade019ccb5d99085f96e28b87373e3938f47d
laipuran <55612059+laipuran@users.noreply.github.com>
2022-08-25 15:54:16 +0800

    Tried to fix bugs, and use the new methods according to StrokeProeprty Class etc;

--- AnimationDrawer/Pages/OutputPage.xaml.cs
+++ AnimationDrawer/Pages/OutputPage.xaml.cs
@@ -19,6 +19,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Path = System.IO.Path;
+using static AnimationDrawer.SingleFrame;
 
 namespace AnimationDrawer.Pages
 {
@@ -36,19 +37,35 @@ namespace AnimationDrawer.Pages
 
         private void JsonButton_Click(object sender, RoutedEventArgs e)
         {
-            string fileName = DateTime.Now.ToString();
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\AniDrawer\\" + fileName + ".json";
-            string json = JsonConvert.SerializeObject(App.strokes);
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\AniDrawer\\";
+            string fileName = DateTime.Now.Year.ToString() + "-" + DateTime.Now.Month.ToString() +
+                "-" + DateTime.Now.Day.ToString() + "-" + DateTime.Now.Hour.ToString() +
+                "-" + DateTime.Now.Minute.ToString() + "-" + DateTime.Now.Second.ToString();
+            string path = directory + fileName + ".json";
+
+            List<SingleFrame> current = new();
+
+            foreach (StrokeCollection item in App.strokes)
+            {
+                current.Add(GetSingleFrame(item));
+            }
+
+            string json = JsonConvert.SerializeObject(current);
+
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
 
             File.WriteAllText(path, json);
 #pragma warning disable CS8602 // 解引用可能出现空引用。
-            Process.Start("explorer.exe", Directory.GetParent(path).FullName);
+            Process.Start("explorer.exe", Directory.GetParent(directory).FullName);
 #pragma warning restore CS8602 // 解引用可能出现空引用。
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\OverTop\\HangerWindows\\";
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\AniDrawer\\";
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
@@ -65,12 +82,16 @@ namespace AnimationDrawer.Pages
                 foreach (string filePath in openFileDialog.FileNames)
                 {
 #pragma warning disable CS8600 // 将 null 字面量或可能为 null 的值转换为非 null 类型。
-                    List<StrokeCollection> strokes = JsonConvert.DeserializeObject<List<StrokeCollection>>(File.ReadAllText(filePath));
+                    AnimationPiece strokes = JsonConvert.DeserializeObject<AnimationPiece>(File.ReadAllText(filePath));
 #pragma warning restore CS8600 // 将 null 字面量或可能为 null 的值转换为非 null 类型。
-                    if (strokes is null)
-                        continue;
 
-                    Strokes.Add(strokes);
+                    if (strokes is null)
+                        return;
+
+                    List<StrokeCollection> lsc = new();
+                    //TODO: Add logic to load animation piece
+
+                    Strokes.Add(lsc);
                     MessageTextBlock.Text += "\n" + Path.GetFileNameWithoutExtension(filePath) + " 加载完成";
                 }
             }
@@ -82,12 +103,16 @@ namespace AnimationDrawer.Pages
             {
                 allStrokes.AddRange(stroke);
             }
-            MessageTextBlock.Text += "\n合并完成，切换页面以清空缓存";
+            MessageTextBlock.Text += $"\n合并完成，length = {allStrokes.Count}\n切换页面以清空缓存";
         }
 
         private void InputButton_Click(object sender, RoutedEventArgs e)
         {
-            App.strokes = allStrokes;
+            List<StrokeCollection> current = new();
+            current.Add(new());
+            current.AddRange(allStrokes);
+            App.strokes = current;
+            MessageTextBlock.Text += "\n 导入完成";
         }
     }
 }
