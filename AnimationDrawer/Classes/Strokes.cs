73f0f9ff2caf58c018125deef46675516533e8ef
laipuran <55612059+laipuran@users.noreply.github.com>
2022-08-26 15:14:21 +0800

    Added method

--- AnimationDrawer/Classes/Strokes.cs
+++ AnimationDrawer/Classes/Strokes.cs
@@ -8,12 +8,12 @@ using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 
-namespace AnimationDrawer
+namespace AnimationDrawer.Ink
 {
     public class StrokeProperty
     {
-        DrawingAttributes drawingAttributes = new();
-        List<StylusPoint> stylusPoints = new();
+        public DrawingAttributes drawingAttributes = new();
+        public List<StylusPoint> stylusPoints = new();
 
         public static StrokeProperty GetStrokeProperty(Stroke stroke)
         {
@@ -28,11 +28,11 @@ namespace AnimationDrawer
 
         public static Stroke GetStroke(StrokeProperty strokeProperty)
         {
-            Stroke stroke = new(GetStylusPoints(strokeProperty.stylusPoints), strokeProperty.drawingAttributes);
+            Stroke stroke = new(GetStylusPointsCollection(strokeProperty.stylusPoints), strokeProperty.drawingAttributes);
             return stroke;
         }
 
-        public static StylusPointCollection GetStylusPoints(List<StylusPoint> stylusPoints)
+        public static StylusPointCollection GetStylusPointsCollection(List<StylusPoint> stylusPoints)
         {
             StylusPointCollection points = new();
             foreach (StylusPoint item in stylusPoints)
@@ -45,8 +45,8 @@ namespace AnimationDrawer
 
     public class SingleFrame
     {
-        List<StrokeProperty> strokeProperties = new();
-        ImageSource? backGround;
+        public List<StrokeProperty> strokeProperties = new();
+        public ImageSource? backGround;
 
         public static SingleFrame GetSingleFrame(StrokeCollection strokes)
         {
@@ -72,7 +72,7 @@ namespace AnimationDrawer
         public static StrokeCollection GetStrokes(SingleFrame frame)
         {
             StrokeCollection strokes = new();
-            foreach (var item in frame.strokeProperties)
+            foreach (StrokeProperty item in frame.strokeProperties)
             {
                 strokes.Add(StrokeProperty.GetStroke(item));
             }
@@ -82,6 +82,59 @@ namespace AnimationDrawer
     
     public class AnimationPiece
     {
-        List<SingleFrame> frames = new();
+        public List<SingleFrame> frames = new();
+        // public int FPS
+
+        public static List<SingleFrame> GetFrames(AnimationPiece piece)
+        {
+            return piece.frames;
+        }
+
+        public static List<StrokeCollection> GetCollectionList(AnimationPiece piece)
+        {
+            List<StrokeCollection> strokes = new();
+            foreach (SingleFrame item in piece.frames)
+            {
+                strokes.Add(SingleFrame.GetStrokes(item));
+            }
+            return strokes;
+        }
+
+        public static AnimationPiece GetAnimationPiece(List<SingleFrame> frames)
+        {
+            AnimationPiece piece = new();
+            piece.frames = frames;
+            return piece;
+        }
+
+        public static AnimationPiece GetAnimationPiece(List<StrokeCollection> strokes)
+        {
+            AnimationPiece piece = new AnimationPiece();
+            foreach (StrokeCollection stroke in strokes)
+            {
+                piece.frames.Add(SingleFrame.GetSingleFrame(stroke));
+            }
+            return piece;
+        }
+
+        public static AnimationPiece GetAnimationPiece(List<StrokeCollection> strokes, ImageSource source)
+        {
+            AnimationPiece piece = new AnimationPiece();
+            foreach (StrokeCollection stroke in strokes)
+            {
+                piece.frames.Add(SingleFrame.GetSingleFrame(stroke, source));
+            }
+            return piece;
+        }
+
+        public static AnimationPiece MergeAnimationPieces(List<AnimationPiece> pieces)
+        {
+            AnimationPiece piece = new();
+            foreach (AnimationPiece item in pieces)
+            {
+                piece.frames.AddRange(item.frames);
+            }
+            return piece;
+        }
     }
 }
